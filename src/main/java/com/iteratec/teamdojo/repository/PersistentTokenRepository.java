d3a86aacd93d2155374aa1e5f4366f40f05a1400
amar.bolkan <amar.bolkan@iteratec.com>
2021-04-22 11:44:56 +0200

    added missing classes

---
+++ src/main/java/com/iteratec/teamdojo/repository/PersistentTokenRepository.java
@@ -0,0 +1,16 @@
+package com.iteratec.teamdojo.repository;
+
+import com.iteratec.teamdojo.domain.PersistentToken;
+import com.iteratec.teamdojo.domain.User;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+}
