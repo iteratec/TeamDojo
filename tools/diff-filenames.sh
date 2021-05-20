39cd0592fbff71cfb77ccc76677c4364f56feba8
Sven Strittmatter <sven.strittmatter@iteratec.com>
2021-05-20 09:56:41 +0200

    Find all files in src not only java
    We want to consider all configs and resources.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- tools/diff-filenames.sh
+++ tools/diff-filenames.sh
@@ -3,9 +3,9 @@
 set -ueo pipefail
 
 #
-# This script compares what files are in two direcotries
+# This script compares what files are in two directories
 #
-# We uyse this to see if we miss some files from v1 in v2.
+# We use this to see if we miss some files from v1 in v2.
 # We assume that the two directories to compares are siblings
 # (in the same parent directory). Eg.:
 # foo/ (COMMON_BASE_DIR)
@@ -31,13 +31,15 @@ BASE_DIR_A=$(echo "${DIR_A}" | sed "s|${COMMON_BASE_DIR}||" | awk -F/ ' { print 
 BASE_DIR_B=$(echo "${DIR_B}" | sed "s|${COMMON_BASE_DIR}||" | awk -F/ ' { print $1 } ')
 
 find_files() {
-    find "${1}/src" -type f -name '*.java' | \
+    find "${1}/src" -type f | \
     # We strip the base dir so.
     sed "s|${COMMON_BASE_DIR}${2}/||" | \
     # We strip the different base package names.
     sed "s|com/iteratec/teamdojo|BASEPACKAGE|" | \
     sed "s|de/otto/teamdojo|BASEPACKAGE|"
 }
+
+
 
 # Sort and diff the found files.
 diff \
