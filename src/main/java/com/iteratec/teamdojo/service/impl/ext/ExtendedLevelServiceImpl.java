0a8cd1c748b8fff2eaa7be66fcc216c82059211a
Amar Bolkan <amar.bolkan@iteratec.com>
2021-05-25 11:39:56 +0200

    Add ExtendedLevelService and it's implementation

---
+++ src/main/java/com/iteratec/teamdojo/service/impl/ext/ExtendedLevelServiceImpl.java
@@ -0,0 +1,33 @@
+package com.iteratec.teamdojo.service.impl.ext;
+
+import com.iteratec.teamdojo.repository.LevelRepository;
+import com.iteratec.teamdojo.repository.ext.ExtendedLevelRepository;
+import com.iteratec.teamdojo.service.dto.LevelDTO;
+import com.iteratec.teamdojo.service.ext.ExtendedLevelService;
+import com.iteratec.teamdojo.service.impl.LevelServiceImpl;
+import com.iteratec.teamdojo.service.mapper.LevelMapper;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class ExtendedLevelServiceImpl extends LevelServiceImpl implements ExtendedLevelService {
+
+    private final Logger log = LoggerFactory.getLogger(ExtendedLevelServiceImpl.class);
+
+    private ExtendedLevelRepository levelRepository;
+    private LevelMapper levelMapper;
+
+    public ExtendedLevelServiceImpl(ExtendedLevelRepository levelRepository, LevelMapper levelMapper) {
+        super(levelRepository, levelMapper);
+        this.levelRepository = levelRepository;
+        this.levelMapper = levelMapper;
+    }
+
+    @Override
+    public Page<LevelDTO> findByIdIn(List<Long> levelIds, Pageable pageable) {
+        log.debug("Request to get Levels by level Ids: {}", levelIds);
+        return levelRepository.findByIdIn(levelIds, pageable).map(levelMapper::toDto);
+    }
+}
