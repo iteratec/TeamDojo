c038c7296e4eb3dba3719d60bcedaa234625c392
Amar Bolkan <amar.bolkan@iteratec.com>
2021-05-27 12:27:29 +0200

    Adds @Service and @Transactional tags to ExtendedSkillQueryService class

--- src/main/java/com/iteratec/teamdojo/service/ext/ExtendedSkillQueryService.java
+++ src/main/java/com/iteratec/teamdojo/service/ext/ExtendedSkillQueryService.java
@@ -10,7 +10,11 @@ import javax.persistence.criteria.JoinType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(readOnly = true)
 public class ExtendedSkillQueryService extends SkillQueryService {
 
     private final Logger log = LoggerFactory.getLogger(ExtendedSkillQueryService.class);
