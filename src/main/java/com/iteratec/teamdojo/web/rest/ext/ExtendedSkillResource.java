713590cb5780a1c3da4f45707858fe23dd3d1517
Amar Bolkan <amar.bolkan@iteratec.com>
2021-06-03 12:14:47 +0200

    Add ExtendedSkillResource

---
+++ src/main/java/com/iteratec/teamdojo/web/rest/ext/ExtendedSkillResource.java
@@ -0,0 +1,41 @@
+package com.iteratec.teamdojo.web.rest.ext;
+
+import com.iteratec.teamdojo.repository.SkillRepository;
+import com.iteratec.teamdojo.service.SkillQueryService;
+import com.iteratec.teamdojo.service.dto.SkillDTO;
+import com.iteratec.teamdojo.service.dto.ext.SkillRateDTO;
+import com.iteratec.teamdojo.service.ext.ExtendedSkillService;
+import com.iteratec.teamdojo.web.rest.SkillResource;
+import com.iteratec.teamdojo.web.rest.ext.util.CustomHeaderUtil;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v2")
+public class ExtendedSkillResource extends SkillResource {
+
+    private final Logger log = LoggerFactory.getLogger(ExtendedSkillResource.class);
+    private final ExtendedSkillService skillService;
+    private static final String ENTITY_NAME = "skill";
+
+    public ExtendedSkillResource(ExtendedSkillService skillService, SkillRepository skillRepository, SkillQueryService skillQueryService) {
+        super(skillService, skillRepository, skillQueryService);
+        this.skillService = skillService;
+    }
+
+    /**
+     * POST /skill/:id : update the "id" skill.
+     *
+     * @param id the id of the skillDTO to update
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @PostMapping("/skills/{id}/vote")
+    public ResponseEntity<SkillDTO> createVote(@PathVariable Long id, @Valid @RequestBody SkillRateDTO rateDto) {
+        log.debug("REST request to create a new vote for Skill : {}", id);
+        SkillDTO result = skillService.createVote(id, rateDto.getRateScore());
+        return ResponseEntity.ok().headers(CustomHeaderUtil.createEntityUpdateAlert(ENTITY_NAME, id.toString())).body(result);
+    }
+}
