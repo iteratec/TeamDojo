45833532da555585c932a8e5ea5e4840abd1991a
Sven Strittmatter <sven.strittmatter@iteratec.com>
2021-06-17 18:21:50 +0200

    Add all the code for persistent audit events from v1
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

---
+++ src/main/java/com/iteratec/teamdojo/config/ext/CustomConstants.java
@@ -0,0 +1,10 @@
+package com.iteratec.teamdojo.config.ext;
+
+public final class CustomConstants {
+
+    public static final String ANONYMOUS_USER = "anonymoususer";
+
+    private CustomConstants() {
+        super();
+    }
+}
