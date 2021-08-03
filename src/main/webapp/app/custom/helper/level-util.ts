iteratec/TeamDojo

commit b7a5cccfee28620cf06e0d2e0bf3391421793249

Add //@ts-ignore to suppress linting error
After migrating the level-util.ts file, the linter throws
following error: "TS2345: Argument of type 'ILevel | undefined' is
not assignable to parameter of type 'ILevel'.   Type 'undefined'
is not assignable to type 'ILevel'."

This seems to arise from calling the pop() function on levels.
However the documentation states that undefined is only returned
if the array is empty. This won't happen in this case, as the
length of the levels is checked to be greater then 0 before
calling pop().

src/main/webapp/app/custom/helper/level-util.ts
@@ -2,11 +2,14 @@ import { ILevel } from 'app/entities/level/level.model';
 
 export const sortLevels = (levels: ILevel[]) => {
   const sortedLevels: ILevel[] = [];
-  if (levels && levels.length > 0) {
+  if (levels.length > 0) {
     const rootLevelIndex = levels.findIndex((level: ILevel) => level.dependsOn === undefined || level.dependsOn === null);
+
+    // @ts-ignore
     sortedLevels.unshift(rootLevelIndex === -1 ? levels.pop() : levels.splice(rootLevelIndex, 1)[0]);
     while (levels.length > 0) {
-      const nextLevelIndex = levels.findIndex((level: ILevel) => level.dependsOn.id === sortedLevels[0].id);
+      const nextLevelIndex = levels.findIndex((level: ILevel) => level.dependsOn?.id === sortedLevels[0].id);
+      // @ts-ignore
       sortedLevels.unshift(nextLevelIndex === -1 ? levels.pop() : levels.splice(nextLevelIndex, 1)[0]);
     }
   }

commit b8a5452798227ee9f2d5aa7b238aaec95145f3bb

Remove //@ts-ignore in level-utils.ts, fix resulting error
Removes the //@ts-ignore comment introduced in commit:
b7a5cccfee28620cf06e0d2e0bf3391421793249

Reason is the linter throwing following error:
"Do not use "@ts-ignore" because it alters compilation errors
@typescript-eslint/ban-ts-comment"

After removing //@ts-ignore the following error occurs:
TS2345: Argument of type 'ILevel | undefined' is not assignable
to parameter of type 'ILevel'.   Type 'undefined' is not
assignable to type 'ILevel'.

src/main/webapp/app/custom/helper/level-util.ts
@@ -1,19 +1,21 @@
-/*
 import { ILevel } from 'app/entities/level/level.model';
 
-
-export const sortLevels = (levels: ILevel[]) : ILevel[] => {
+export const sortLevels = (levels: ILevel[]): ILevel[] => {
   const sortedLevels: ILevel[] = [];
   if (levels.length > 0) {
     const rootLevelIndex = levels.findIndex((level: ILevel) => level.dependsOn === undefined || level.dependsOn === null);
+    let currentLevel: ILevel | undefined = rootLevelIndex === -1 ? levels.pop() : levels.splice(rootLevelIndex, 1)[0];
+    if (currentLevel) {
+      sortedLevels.unshift(currentLevel);
+    }
 
-    // @ts-ignore
-    sortedLevels.unshift(rootLevelIndex === -1 ? levels.pop() : levels.splice(rootLevelIndex, 1)[0]);
     while (levels.length > 0) {
       const nextLevelIndex = levels.findIndex((level: ILevel) => level.dependsOn?.id === sortedLevels[0].id);
-      // @ts-ignore
-      sortedLevels.unshift(nextLevelIndex === -1 ? levels.pop() : levels.splice(nextLevelIndex, 1)[0]);
+      currentLevel = nextLevelIndex === -1 ? levels.pop() : levels.splice(nextLevelIndex, 1)[0];
+      if (currentLevel) {
+        sortedLevels.unshift(currentLevel);
+      }
     }
   }
   return sortedLevels;
-}; */
+};