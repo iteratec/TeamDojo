284f0d0aad3a97617fcfae09fc5e732126cb4651
Amar Bolkan <amar.bolkan@iteratec.com>
2021-08-05 15:32:21 +0200

    Add teams-selection component and resolver from V1

---
+++ src/main/webapp/app/custom/teams-selection/teams-selection.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { TeamsSelectionService } from './teams-selection.service';
+import { TeamsService } from 'app/teams/teams.service';
+import { ITeam } from 'app/shared/model/team.model';
+import { Router } from '@angular/router';
+import { TeamsEditComponent } from 'app/shared/teams/teams-edit.component';
+
+@Component({
+  selector: 'jhi-teams-selection',
+  templateUrl: './teams-selection.component.html',
+  styleUrls: ['./teams-selection.scss'],
+})
+export class TeamsSelectionComponent implements OnInit {
+  highlightedTeam: ITeam = null;
+  selectedTeam: ITeam;
+  teams: ITeam[] = [];
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private teamsSelectionService: TeamsSelectionService,
+    private teamsService: TeamsService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {
+    this.teamsService.query().subscribe(teams => {
+      this.teams = teams.body.filter(team => !team.expired).sort((a, b) => a.shortName.localeCompare(b.shortName));
+    });
+    this.teamsSelectionService.query().subscribe(selectedTeam => {
+      this.selectedTeam = this.highlightedTeam = selectedTeam;
+    });
+  }
+
+  selectTeam(team: ITeam) {
+    this.highlightedTeam = team;
+    this.teamsSelectionService.selectedTeam = team;
+    this.activeModal.close('Team selected');
+    this.router.navigate(['teams', team.shortName]);
+  }
+
+  deselectTeam() {
+    this.highlightedTeam = null;
+    this.teamsSelectionService.selectedTeam = null;
+    this.activeModal.close('No team selected');
+    this.router.navigate(['']);
+  }
+
+  createNewTeam(): NgbModalRef {
+    this.activeModal.close('Create new Team');
+    const modalRef = this.modalService.open(TeamsEditComponent, { size: 'lg' });
+    (<TeamsEditComponent>modalRef.componentInstance).team = { official: false, pureTrainingTeam: true };
+    modalRef.result.then(team => {
+      this.selectedTeam = team;
+      this.highlightedTeam = team;
+      this.teamsSelectionService.selectedTeam = team;
+      this.teamsSelectionService.query().subscribe();
+      this.router.navigate(['/teams/', (<ITeam>team).shortName]);
+    });
+    return modalRef;
+  }
+
+  cancelTeamSelection() {
+    this.activeModal.dismiss('Team selected cancelled');
+  }
+}
---
+++ src/main/webapp/app/custom/teams-selection/teams-selection.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, OnInit } from '@angular/core';
+import { ITeam } from 'app/shared/model/team.model';
+import { TeamsService } from 'app/teams/teams.service';
+import { LocalStorageService } from 'ngx-webstorage';
+import { TeamSkillService } from 'app/entities/team-skill';
+import { Observable, empty, of } from 'rxjs';
+import { catchError, flatMap, map, tap } from 'rxjs/operators';
+
+const TEAM_STORAGE_KEY = 'selectedTeamId';
+
+@Injectable()
+export class TeamsSelectionService {
+  private _selectedTeam: ITeam = null;
+
+  constructor(private teamsService: TeamsService, private teamSkillService: TeamSkillService, private storage: LocalStorageService) {
+    this.query();
+  }
+
+  query(): Observable<ITeam> {
+    const teamIdStr = this.storage.retrieve(TEAM_STORAGE_KEY);
+    if (teamIdStr !== null && !isNaN(Number(teamIdStr))) {
+      return this.teamsService.find(teamIdStr).pipe(
+        tap(result => {
+          this._selectedTeam = result.body || null;
+        }),
+        catchError(err => {
+          this.selectedTeam = null;
+          return empty();
+        }),
+        flatMap((result: any) => {
+          return this.teamSkillService.query({ 'teamId.equals': result.body.id }).pipe(
+            tap((teamSkillRes: any) => {
+              this._selectedTeam.skills = teamSkillRes.body || [];
+            }),
+            map(() => result.body)
+          );
+        })
+      );
+    }
+    return of(this._selectedTeam);
+  }
+
+  get selectedTeam() {
+    return this._selectedTeam;
+  }
+
+  set selectedTeam(team: ITeam) {
+    this._selectedTeam = team;
+    if (team !== null) {
+      this.storage.store(TEAM_STORAGE_KEY, this._selectedTeam.id.toString());
+    } else {
+      this.storage.clear(TEAM_STORAGE_KEY);
+    }
+  }
+}
