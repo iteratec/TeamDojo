7404e66a58ef3512df13e413ec604eed08bc67f3
Amar Bolkan <amar.bolkan@iteratec.com>
2021-08-09 11:48:07 +0200

    Add missing return type

--- src/main/webapp/app/custom/teams-selection/teams-selection.resolve.ts
+++ src/main/webapp/app/custom/teams-selection/teams-selection.resolve.ts
@@ -1,12 +1,16 @@
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
 import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selection.service';
+import { ITeam } from 'app/entities/team/team.model';
 
 @Injectable()
 export class TeamsSelectionResolve implements Resolve<any> {
   constructor(private teamsSelectionService: TeamsSelectionService) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ITeam> {
     return this.teamsSelectionService.query();
   }
 }
