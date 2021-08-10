d252d25dbc05a6b295838c061e064bebc8e26a79
Amar Bolkan <amar.bolkan@iteratec.com>
2021-08-10 10:18:17 +0200

    Add background component from V1

---
+++ src/main/webapp/app/custom/shared/background/background.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { TeamsSelectionService } from 'app/shared/teams-selection/teams-selection.service';
+import { ITeam } from '../model/team.model';
+import { ITeamSkill } from 'app/shared/model/team-skill.model';
+import { ISkill } from 'app/shared/model/skill.model';
+import { IBadge } from 'app/shared/model/badge.model';
+
+@Component({
+  selector: 'jhi-background-component',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.scss'],
+})
+export class BackgroundComponent {
+  @Input() team: ITeam;
+  @Input() teamSkills: ITeamSkill[] = [];
+  @Input() skills: ISkill[] = [];
+  @Input() badges: IBadge[] = [];
+  constructor(private teamsSelectionService: TeamsSelectionService) {}
+
+  get currentTeam(): ITeam {
+    return this.team || this.teamsSelectionService.selectedTeam;
+  }
+}
