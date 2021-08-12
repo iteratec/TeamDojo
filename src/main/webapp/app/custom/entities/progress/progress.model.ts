2bb12e0acde7ba837f1fc59e022630a32ff6eb46
Amar Bolkan <amar.bolkan@iteratec.com>
2021-08-12 14:13:30 +0200

    Add Progress model from V1

---
+++ src/main/webapp/app/custom/entities/progress/progress.model.ts
@@ -0,0 +1,21 @@
+export interface IProgress {
+  achieved?: number;
+  required?: number;
+  totalScore?: number;
+
+  getPercentage(): number;
+
+  isCompleted(): boolean;
+}
+
+export class Progress implements IProgress {
+  constructor(public achieved?: number, public required?: number, public totalScore?: number) {}
+
+  public getPercentage(): number {
+    return this.required !== 0 ? (this.achieved / this.totalScore) * 100 : 0;
+  }
+
+  public isCompleted(): boolean {
+    return this.achieved >= this.required;
+  }
+}
