e37fc81e283867870efc2e81ebc405839fbf9ef7
Amar Bolkan <amar.bolkan@iteratec.com>
2021-08-12 11:26:09 +0200

    Fix compiler errors in background.component.ts
    teamsSelectionService.selectedTeam and team can be null or
    undefined. If thats the case this method now returns a new Team()
    object.

--- src/main/webapp/app/custom/shared/background/background.component.ts
+++ src/main/webapp/app/custom/shared/background/background.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { TeamsSelectionService } from 'app/shared/teams-selection/teams-selection.service';
-import { ITeam } from '../model/team.model';
-import { ITeamSkill } from 'app/shared/model/team-skill.model';
-import { ISkill } from 'app/shared/model/skill.model';
-import { IBadge } from 'app/shared/model/badge.model';
+import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selection.service';
+import { ITeam, Team } from 'app/entities/team/team.model';
+import { ITeamSkill } from 'app/entities/team-skill/team-skill.model';
+import { ISkill } from 'app/entities/skill/skill.model';
+import { IBadge } from 'app/entities/badge/badge.model';
 
 @Component({
   selector: 'jhi-background-component',
@@ -11,13 +11,14 @@ import { IBadge } from 'app/shared/model/badge.model';
   styleUrls: ['./background.scss'],
 })
 export class BackgroundComponent {
-  @Input() team: ITeam;
+  @Input() team?: ITeam;
   @Input() teamSkills: ITeamSkill[] = [];
   @Input() skills: ISkill[] = [];
   @Input() badges: IBadge[] = [];
   constructor(private teamsSelectionService: TeamsSelectionService) {}
 
-  /*get currentTeam(): ITeam {
-    return this.team || this.teamsSelectionService.selectedTeam;
-  }*/
+  get currentTeam(): ITeam {
+    const currTeam = this.team ?? this.teamsSelectionService.selectedTeam;
+    return currTeam ? currTeam : new Team();
+  }
 }
