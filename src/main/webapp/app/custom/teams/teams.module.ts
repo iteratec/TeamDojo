7aea08f43c75f19bb28ca54fcc0f9bf3405a721e
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-06 11:46:16 +0200

    Fix imports

--- src/main/webapp/app/custom/teams/teams.module.ts
+++ src/main/webapp/app/custom/teams/teams.module.ts
@@ -1,49 +1,43 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-markdown';
 
-import { TeamdojoSharedModule } from 'app/shared';
-import { TEAMS_ROUTES, TeamsComponent } from './';
-import { TeamsAchievementsComponent } from './teams-achievements.component';
-import { TeamsSkillsComponent } from './teams-skills.component';
-import { TeamsSelectionComponent } from 'app/shared/teams-selection/teams-selection.component';
-import { TeamAndTeamSkillResolve } from './teams.route';
-import { TeamsService } from './teams.service';
-import { TeamsSkillsService } from './teams-skills.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TeamsAchievementsService } from 'app/teams/teams-achievements.service';
-import { SkillDetailsComponent } from 'app/teams/skill-details/skill-details.component';
-import { SkillDetailsInfoComponent } from 'app/shared/skill-details/skill-details-info.component';
-import { SkillDetailsCommentsComponent } from 'app/shared/skill-details/skill-details-comments.component';
-import { SkillDetailsRatingComponent } from 'app/teams/skill-details/skill-details-rating/skill-details-rating.component';
-import { AllCommentsResolve, AllSkillsResolve, DojoModelResolve, SkillResolve } from 'app/shared/common.resolver';
-import { TeamsEditComponent } from 'app/shared/teams/teams-edit.component';
+import { TeamAndTeamSkillResolve, TEAMS_ROUTES } from 'app/custom/teams/teams.route';
+import { TeamsComponent } from 'app/custom/teams/teams.component';
+import { TeamsAchievementsComponent } from 'app/custom/teams/teams-achievements/teams-achievements.component';
+import { TeamsSkillsComponent } from 'app/custom/teams/teams-skills/teams-skills.component';
+import { TeamsEditComponent } from 'app/custom/teams/teams-edit/teams-edit.component';
+import { AllCommentsResolve, AllSkillsResolve, DojoModelResolve, SkillResolve } from 'app/custom/common.resolver';
+import { TeamsService } from 'app/custom/teams/teams.service';
+import { CustomSharedModule } from 'app/custom/shared/custom-shared.module';
 
 @NgModule({
-  imports: [TeamdojoSharedModule, RouterModule.forChild(TEAMS_ROUTES), NgbModule, MarkdownModule.forChild()],
+  imports: [CustomSharedModule, RouterModule.forChild(TEAMS_ROUTES), NgbModule, MarkdownModule.forChild()],
   declarations: [
     TeamsComponent,
     TeamsAchievementsComponent,
     TeamsSkillsComponent,
-    TeamsSelectionComponent,
     TeamsEditComponent,
+    /*TeamsSelectionComponent,
     SkillDetailsComponent,
     SkillDetailsInfoComponent,
     SkillDetailsCommentsComponent,
-    SkillDetailsRatingComponent,
+    SkillDetailsRatingComponent,*/
   ],
-  entryComponents: [TeamsSelectionComponent, TeamsEditComponent],
+  // @Fixme add this back to entrycomponents: TeamsSelectionComponent
+  entryComponents: [TeamsEditComponent],
   providers: [
     DojoModelResolve,
     TeamsService,
-    TeamsSkillsService,
-    TeamsAchievementsService,
+    /*TeamsSkillsService,
+    TeamsAchievementsService,*/
     TeamAndTeamSkillResolve,
     SkillResolve,
     AllSkillsResolve,
     AllCommentsResolve,
   ],
-  exports: [SkillDetailsInfoComponent, SkillDetailsCommentsComponent],
+  //exports: [SkillDetailsInfoComponent, SkillDetailsCommentsComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TeamsModule {}
