cd72743a46e4deffcebdac322c592dd5f24679f7
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-06 11:02:53 +0200

    Add teams.route.ts from V1

---
+++ src/main/webapp/app/custom/teams/teams.route.ts
@@ -0,0 +1,74 @@
+import { ActivatedRouteSnapshot, Resolve, Route, Router, RouterStateSnapshot } from '@angular/router';
+
+import { TeamsComponent } from './';
+import { TeamsService } from 'app/teams/teams.service';
+import { Team } from 'app/shared/model/team.model';
+import { Injectable } from '@angular/core';
+import { SkillDetailsComponent } from 'app/teams/skill-details/skill-details.component';
+import { TeamSkillService } from 'app/entities/team-skill';
+import { TeamsSelectionResolve } from 'app/shared/teams-selection/teams-selection.resolve';
+import { AllCommentsResolve, AllSkillsResolve, AllTrainingsResolve, DojoModelResolve, SkillResolve } from 'app/shared/common.resolver';
+import { flatMap, map } from 'rxjs/operators';
+
+@Injectable()
+export class TeamAndTeamSkillResolve implements Resolve<any> {
+  constructor(private teamService: TeamsService, private teamSkillService: TeamSkillService, private router: Router) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const shortName = route.params['shortName'] ? route.params['shortName'] : null;
+    if (shortName) {
+      return this.teamService
+        .query({
+          'shortName.equals': shortName,
+        })
+        .pipe(
+          flatMap(teamResponse => {
+            if (teamResponse.body.length === 0) {
+              this.router.navigate(['/error']);
+            }
+            const team = teamResponse.body[0];
+            return this.teamSkillService.query({ 'teamId.equals': team.id }).pipe(
+              map(teamSkillResponse => {
+                team.skills = teamSkillResponse.body;
+                return team;
+              })
+            );
+          })
+        );
+    }
+    return new Team();
+  }
+}
+
+export const TEAMS_ROUTES: Route[] = [
+  {
+    path: 'teams/:shortName',
+    component: TeamsComponent,
+    resolve: {
+      dojoModel: DojoModelResolve,
+      team: TeamAndTeamSkillResolve,
+      skills: AllSkillsResolve,
+    },
+    data: {
+      authorities: [],
+      pageTitle: 'teamdojoApp.teams.home.title',
+    },
+  },
+  {
+    path: 'teams/:shortName/skills/:skillId',
+    component: SkillDetailsComponent,
+    resolve: {
+      dojoModel: DojoModelResolve,
+      team: TeamAndTeamSkillResolve,
+      skill: SkillResolve,
+      skills: AllSkillsResolve,
+      comments: AllCommentsResolve,
+      selectedTeam: TeamsSelectionResolve,
+      trainings: AllTrainingsResolve,
+    },
+    data: {
+      authorities: [],
+      pageTitle: 'teamdojoApp.teams.skills.title',
+    },
+  },
+];
