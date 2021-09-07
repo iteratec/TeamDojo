af9acb7a5a88a3b6f947b366550feac4d2eefd16
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-07 14:25:31 +0200

    Remove TeamDojoSharedLibsModule import from CustomSharedModule

--- src/main/webapp/app/custom/shared/custom-shared.module.ts
+++ src/main/webapp/app/custom/shared/custom-shared.module.ts
@@ -5,9 +5,11 @@ import { SharedLibsModule } from 'app/shared/shared-libs.module';
 import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selection.service';
 import { TeamsService } from 'app/custom/teams/teams.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { TeamdojoSharedCommonModule } from 'app/custom/shared/shared-common.module';
+import { TeamdojoSharedLibsModule } from 'app/custom/shared/shared-libs.module';
 
 @NgModule({
-  imports: [SharedLibsModule, TranslateModule],
+  imports: [SharedLibsModule, TranslateModule, TeamdojoSharedCommonModule],
   declarations: [BackgroundComponent, TeamsStatusComponent],
   exports: [BackgroundComponent, TeamsStatusComponent, TranslateModule],
   providers: [TeamsSelectionService, TeamsService],
