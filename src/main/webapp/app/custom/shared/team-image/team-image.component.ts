cc07a899b3fb64763d828a4eb9f62bedc304596b
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-07 13:01:21 +0200

    Add team-image component from V1

---
+++ src/main/webapp/app/custom/shared/team-image/team-image.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { ITeam } from 'app/shared/model/team.model';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'jhi-team-image',
+  templateUrl: './team-image.component.html',
+  styleUrls: ['./team-image.scss'],
+})
+export class TeamImageComponent {
+  @Input() team: ITeam;
+  @Input() size = '50px';
+  @Input() imageSize = 'large';
+  @Input() hasPlaceholder = true;
+  @Input() hasBorder = true;
+  @Input() hasOverlay = false;
+  @Input() showExpiredLabel = true;
+  constructor() {}
+}
