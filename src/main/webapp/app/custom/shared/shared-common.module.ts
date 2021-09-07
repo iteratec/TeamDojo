96b97da460b94782813228f6625528b0e0b07f75
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-07 14:01:28 +0200

    Import TruncateStringPipe in TeamdojoSharedCommonModule
    Remove AlertErrorComponent, AlertComponent and
    FindLanguageFromKeyPipe from TeamdojoSharedCommonModule
    as they are already declared in SharedModule.

--- src/main/webapp/app/custom/shared/shared-common.module.ts
+++ src/main/webapp/app/custom/shared/shared-common.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { TeamdojoSharedLibsModule } from 'app/custom/shared/shared-libs.module';
-import { AlertComponent } from 'app/shared/alert/alert.component';
-import { AlertErrorComponent } from 'app/shared/alert/alert-error.component';
+
 //import { DojoTranslateDirective } from 'app/custom/shared/language/dojoTranslate.directive';
 import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selection.service';
 import { TeamsSelectionResolve } from 'app/custom/teams-selection/teams-selection.resolve';
-import { FindLanguageFromKeyPipe } from 'app/shared/language/find-language-from-key.pipe';
 import { TeamImageComponent } from 'app/custom/shared/team-image/team-image.component';
+import { SharedModule } from 'app/shared/shared.module';
+import { TruncateStringPipe } from 'app/custom/shared/pipe/truncate-string.pipe';
 
 @NgModule({
-  imports: [TeamdojoSharedLibsModule],
+  imports: [TeamdojoSharedLibsModule, SharedModule],
   declarations: [
-    AlertErrorComponent,
-    AlertComponent,
-    FindLanguageFromKeyPipe,
     TeamImageComponent,
+    TruncateStringPipe,
     /*SkillFilterPipe,
     DojoTranslateDirective,
     SkillSortPipe,
     ImageUrlPipe,
-    TruncateStringPipe,
     AchievementItemComponent,
     NumberInputComponent,
     SkillScoreComponent,
@@ -29,15 +26,12 @@ import { TeamImageComponent } from 'app/custom/shared/team-image/team-image.comp
   providers: [TeamsSelectionService, TeamsSelectionResolve],
   exports: [
     TeamdojoSharedLibsModule,
-    FindLanguageFromKeyPipe,
-    AlertComponent,
-    AlertErrorComponent,
     TeamImageComponent,
+    TruncateStringPipe,
     /*SkillFilterPipe,
     DojoTranslateDirective,
     SkillSortPipe,
     ImageUrlPipe,
-    TruncateStringPipe,
     AchievementItemComponent,
     NumberInputComponent,
     SkillScoreComponent,
