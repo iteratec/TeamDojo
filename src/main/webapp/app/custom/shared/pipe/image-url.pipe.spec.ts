1595359545750472d26cab1c16573c817582f151
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-13 11:52:35 +0200

    Refactor image-url tests for the parameters size and cacheBuster
    Instead of matching a hardcoded string, regular expressions
    are being used.

--- src/main/webapp/app/custom/shared/pipe/image-url.pipe.spec.ts
+++ src/main/webapp/app/custom/shared/pipe/image-url.pipe.spec.ts
@@ -7,28 +7,23 @@ describe('ImageUrlPipe', () => {
     expect(imageUrlPipe.transform(undefined, 'size', 'cacheBuster')).toBe('');
   });
 
-  it('should return  a uri string starting with /api/images/4/ if imageId is equal to 4', () => {
-    expect(imageUrlPipe.transform(4, '10', undefined)).toMatch(/^\/api\/images\/4\/.*/);
+  it('should return  a uri string starting with /api/images/4/content if imageId is equal to 4', () => {
+    expect(imageUrlPipe.transform(4, '10', undefined)).toMatch(/^\/api\/images\/4\/content.*/);
   });
 
   it('should return a uri string containing the query size=10 if the parameter size is equal to "10"', () => {
-    expect(imageUrlPipe.transform(4, '10', undefined));
+    expect(imageUrlPipe.transform(4, '10', undefined)).toMatch(
+      /.*content\?(size=10(&[^=?&]+=[^=?&]+)*|[^=?&]+=[^=?&]+(&[^=?&]+=[^=?&]+)*&size=10(&[^=?&]+=[^=?&]+)*)$/
+    );
   });
 
-  it(
-    'should return the string /api/images/4/content?size=10 if ' +
-      'imageId is equal to "4", size is equal to "10" and cacheBuster ' +
-      'is undefined',
-    () => {
-      expect(imageUrlPipe.transform('4', '10', undefined)).toBe('/api/images/4/content?size=10');
-    }
-  );
+  it('should return a uri string not containing the query "cacheBuster" if the parameter cacheBuster is omitted', () => {
+    expect(imageUrlPipe.transform(4, '10')).toMatch(/.*content\?((?!.*cacheBuster=)[^=?&]+=[^=?&]+(&[^=?&]+=[^=?&]+)*)$/);
+  });
 
-  it(
-    'should return the string /api/images/4/content?size=10&cacheBuster=cacheBuster if ' +
-      'imageId is equal to 4, size is equal to "10" and cacheBuster equal to "cacheBuster"',
-    () => {
-      expect(imageUrlPipe.transform(4, '10', 'cacheBuster')).toBe('/api/images/4/content?size=10&cacheBuster=cacheBuster');
-    }
-  );
+  it('should return a uri string containing the query cacheBuster=true if the parameter cacheBuster is equal to "true"', () => {
+    expect(imageUrlPipe.transform(4, '10', 'true')).toMatch(
+      /.*content\?(cacheBuster=true(&[^=?&]+=[^=?&]+)*|[^=?&]+=[^=?&]+(&[^=?&]+=[^=?&]+)*&cacheBuster=true(&[^=?&]+=[^=?&]+)*)$/
+    );
+  });
 });
