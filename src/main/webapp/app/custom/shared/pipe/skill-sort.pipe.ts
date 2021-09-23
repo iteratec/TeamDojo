c675676a79d27ac668333c5cc7523f00fa856598
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-23 15:30:07 +0200

    Add missing return types

--- src/main/webapp/app/custom/shared/pipe/skill-sort.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/skill-sort.pipe.ts
@@ -1,20 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Skill } from 'app/entities/skill/skill.model';
 
 @Pipe({ name: 'skillSort' })
 export class SkillSortPipe implements PipeTransform {
-  transform(skills = [], sortProperty) {
+  transform(skills: Skill[], sortProperty: any): Skill[] {
     const sortPropertyNullSafe = this._defaultString(sortProperty);
     const reverse = ['score', 'rateCount'].includes(sortPropertyNullSafe) ? -1 : 1;
     return sortProperty
-      ? Array.from(skills).sort((skill1, skill2) => {
-          return (
+      ? Array.from(skills).sort(
+          (skill1, skill2) =>
             reverse * this._defaultString(skill1[sortPropertyNullSafe]).localeCompare(this._defaultString(skill2[sortPropertyNullSafe]))
-          );
-        })
+        )
       : skills;
   }
 
-  _defaultString(smth) {
+  _defaultString(smth: number): string {
     return smth || smth === 0 ? String(smth) : String();
   }
 }
