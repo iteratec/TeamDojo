6b8f023f1564ce0d5abf64928f081dc8f285d431
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-23 12:09:19 +0200

    Add skill-rate model from V1
    It looks like this model was added manually in V1. The SkillRateDTO
    was already migrated and put into a custom folder. As such it
    seems to be appropriate to  migrate the skill-rate model
    without moving it into the teamDojo_v2.jdl.

---
+++ src/main/webapp/app/custom/entities/skill-rate/skill-rate.model.ts
@@ -0,0 +1,9 @@
+export interface ISkillRate {
+  skillId?: number;
+  rateScore?: number;
+  rateCount?: number;
+}
+
+export class SkillRate implements ISkillRate {
+  constructor(public skillId?: number, public rateScore?: number, public rateCount?: number) {}
+}
