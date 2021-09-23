281b80b9432b6821c8bd07c14f1d72bdf1661075
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-23 13:28:13 +0200

    Add achievable-skill.model.ts from V1
    This model seems to have been added manually in V1.
    Due to the changes in AchiveableSkill already being migrated
    into their own Custom Version, it seems to make sense to
    migrate this model without moving it into the
    teamDojo_v2.jdl.

---
+++ src/main/webapp/app/custom/entities/achievable-skill/achievable-skill.model.ts
@@ -0,0 +1,35 @@
+import { Moment } from 'moment';
+import { SkillStatus } from 'app/shared/model/skill-status';
+
+export interface IAchievableSkill {
+  teamSkillId?: number;
+  skillId?: number;
+  title?: string;
+  description?: string;
+  achievedAt?: Moment;
+  verifiedAt?: Moment;
+  vote?: number;
+  voters?: string;
+  irrelevant?: boolean;
+  score?: number;
+  skillStatus?: SkillStatus;
+  rateScore?: number;
+  rateCount?: number;
+}
+
+export class AchievableSkill implements IAchievableSkill {
+  constructor(
+    public skillId?: number,
+    public title?: string,
+    public description?: string,
+    public achievedAt?: Moment,
+    public verifiedAt?: Moment,
+    public vote?: number,
+    public voters?: string,
+    public irrelevant?: boolean,
+    public score?: number,
+    public skillStatus?: SkillStatus,
+    public rateScore?: number,
+    public rateCount?: number
+  ) {}
+}
