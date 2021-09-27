e5be0ea015dbc87bcdbb258ecb57b76379690472
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-27 14:02:15 +0200

    Fix compiler errors  in skill-filter.pipe.ts

--- src/main/webapp/app/custom/shared/pipe/skill-filter.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/skill-filter.pipe.ts
@@ -1,14 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { ILevelSkill } from 'app/shared/model/level-skill.model';
-import { IBadgeSkill } from 'app/shared/model/badge-skill.model';
+import { ISkill } from 'app/entities/skill/skill.model';
 
 @Pipe({ name: 'skillFilter' })
 export class SkillFilterPipe implements PipeTransform {
-  transform(skills, searchString: string) {
-    return searchString
-      ? (skills || []).filter(skill => {
-          return (skill.title || skill.skillTitle || '').toLowerCase().includes(searchString.toLowerCase());
-        })
-      : skills;
+  transform(skills: ISkill[], searchString: string) {
+    return (skills || []).filter(skill => {
+      return (skill.title || skill.title || '').toLowerCase().includes(searchString.toLowerCase());
+    });
   }
 }
