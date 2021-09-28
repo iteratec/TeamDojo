7ae72b2c1104895a9dde4b0b248bd4eab26291ea
Amar Bolkan <amar.bolkan@iteratec.com>
2021-09-28 12:24:21 +0200

    Add overview-skill-details.component from V1

---
+++ src/main/webapp/app/custom/overview/skills/skill-details/overview-skill-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ILevel } from 'app/shared/model/level.model';
+import { TeamsSkillsService } from 'app/teams/teams-skills.service';
+import { SkillDetailsComponentParent } from 'app/shared/skill-details/skill-details.component';
+import { ISkillRate } from 'app/shared/model/skill-rate.model';
+
+@Component({
+  selector: 'jhi-overview-skill-details',
+  templateUrl: './overview-skill-details.component.html',
+  styleUrls: ['./overview-skill-details.scss'],
+})
+export class OverviewSkillDetailsComponent extends SkillDetailsComponentParent implements OnInit {
+  levels: ILevel[];
+
+  constructor(route: ActivatedRoute, teamsSkillsService: TeamsSkillsService) {
+    super(route, teamsSkillsService);
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(({ dojoModel: { teams, badges, levels }, skill, skills, comments, selectedTeam }) => {
+      this.levels = (levels && levels.body ? levels.body : levels) || [];
+      super.setResolvedData({ teams, skill, comments, selectedTeam, badges, skills });
+    });
+  }
+
+  onVoteSubmitted(skillRate: ISkillRate) {
+    for (const skill of this.skills) {
+      if (skillRate.skillId === skill.id) {
+        skill.rateScore = skillRate.rateScore;
+        skill.rateCount = skillRate.rateCount;
+      }
+    }
+  }
+}
