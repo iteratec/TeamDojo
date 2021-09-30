b760a40fa20e883906ba3005a32a7377e375d98e
Robert Seedorff <Robert.Seedorff@iteratec.com>
2021-09-30 12:07:16 +0200

    Migrated Footer Layout from v1

--- src/main/webapp/app/custom/layouts/footer/footer.component.ts
+++ src/main/webapp/app/custom/layouts/footer/footer.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { VERSION, BUILD_TIMESTAMP } from 'app/app.constants';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  version = '';
+
+  constructor() {
+    if (VERSION) {
+      this.version += VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION;
+    }
+    if (BUILD_TIMESTAMP) {
+      this.version += `.${BUILD_TIMESTAMP}`;
+    }
+  }
+}
