8d18b569ffa349abc8c57f6f1842e87b784a888c
Amar Bolkan <amar.bolkan@iteratec.com>
2021-10-15 11:06:29 +0200

    Add TrainingsComponent to custom-shared.module.ts
    Uncomment TrainingsComponent in skill-details-info.component.ts

--- src/main/webapp/app/custom/shared/custom-shared.module.ts
+++ src/main/webapp/app/custom/shared/custom-shared.module.ts
@@ -6,11 +6,13 @@ import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selectio
 import { TeamsService } from 'app/custom/teams/teams.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { TeamdojoSharedCommonModule } from 'app/custom/shared/shared-common.module';
+import { TrainingsAddComponent } from 'app/custom/shared/trainings/trainings-add.component';
 
 @NgModule({
   imports: [SharedLibsModule, TranslateModule, TeamdojoSharedCommonModule],
-  declarations: [BackgroundComponent, TeamsStatusComponent],
+  declarations: [BackgroundComponent, TeamsStatusComponent, TrainingsAddComponent],
   exports: [BackgroundComponent, TeamsStatusComponent, TranslateModule, TeamdojoSharedCommonModule],
   providers: [TeamsSelectionService, TeamsService],
+  entryComponents: [TrainingsAddComponent],
 })
 export class CustomSharedModule {}
