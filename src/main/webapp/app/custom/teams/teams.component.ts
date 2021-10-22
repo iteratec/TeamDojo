8dc0465d94e1546ec76449b24840d39ec31061f3
Amar Bolkan <amar.bolkan@iteratec.com>
2021-10-22 14:25:01 +0200

    Add missing code from V1, initialize class fields

--- src/main/webapp/app/custom/teams/teams.component.ts
+++ src/main/webapp/app/custom/teams/teams.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { TeamSkillService } from 'app/entities/team-skill/service/team-skill.service';
 
 import { IBadge } from 'app/entities/badge/badge.model';
-import { ITeam } from 'app/entities/team/team.model';
+import { ITeam, Team } from 'app/entities/team/team.model';
 import { ITeamSkill } from 'app/entities/team-skill/team-skill.model';
 import { ISkill } from 'app/entities/skill/skill.model';
 import { DataUtils } from 'app/core/util/data-util.service';
@@ -17,10 +17,10 @@ import { DataUtils } from 'app/core/util/data-util.service';
 export class TeamsComponent implements OnInit {
   @Output() changeTeam = new EventEmitter<any>();
 
-  team!: ITeam; // TODO discuss whether this should be ! or | undefined
-  teamSkills!: ITeamSkill[];
-  badges!: IBadge[];
-  skills!: ISkill[];
+  team: ITeam = new Team();
+  teamSkills: ITeamSkill[] = [];
+  badges: IBadge[] = [];
+  skills: ISkill[] = [];
 
   constructor(private dataUtils: DataUtils, private route: ActivatedRoute, private teamSkillService: TeamSkillService) {}
 
@@ -30,8 +30,29 @@ export class TeamsComponent implements OnInit {
       this.team = (teams || []).find((t: ITeam) => t.id === teamFromRoute.id) || teamFromRoute;
       this.teamSkills = team?.skills ? team.skills : [];
       this.badges = (badges?.body ? badges.body : badges) || [];
-      this.skills = (skills?.body ? skills.body : skills) || []; // skills?.body <=> skills && skills.body
+      this.skills = (skills?.body ? skills.body : skills) || [];
     });
     this.changeTeam.emit(this.team);
+  }
+
+  loadTeamSkills(): void {
+    this.teamSkillService.query({ 'teamId.equals': this.team.id }).subscribe(teamSkillResponse => {
+      if (teamSkillResponse.body) {
+        this.team.skills = teamSkillResponse.body;
+        this.teamSkills = teamSkillResponse.body;
+      }
+    });
+  }
+
+  /*byteSize(field): string {
+    return this.dataUtils.byteSize(field);
+  }
+
+  openFile(contentType, field) {
+    return this.dataUtils.openFile(contentType, field);
+  }*/
+
+  previousState(): void {
+    window.history.back();
   }
 }
