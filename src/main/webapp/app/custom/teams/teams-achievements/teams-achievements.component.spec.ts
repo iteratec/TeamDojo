c05a708cdf423d2650dabdb25d537da5bb1ce91a
Amar Bolkan <amar.bolkan@iteratec.com>
2021-10-22 12:51:53 +0200

    Update V1 teams-achievement.component test such that it runs in V2
    The old V1 component tests uses no longer generated TestingModules
    and classes. In order to migrate it from V1 to V2
    necessary adaptions are made.

--- src/main/webapp/app/custom/teams/teams-achievements/teams-achievements.component.spec.ts
+++ src/main/webapp/app/custom/teams/teams-achievements/teams-achievements.component.spec.ts
@@ -1,31 +1,70 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
-import Util from '../../helper/services/Util.service';
+import { Observable, of } from 'rxjs';
+
 import { Team } from 'app/entities/team/team.model';
 import { Badge } from 'app/entities/badge/badge.model';
 import { Dimension } from 'app/entities/dimension/dimension.model';
 import { Level } from 'app/entities/level/level.model';
 import { TeamsAchievementsComponent } from 'app/custom/teams/teams-achievements/teams-achievements.component';
-import { TeamsAchievementsService } from 'app/custom/teams/services/teams-achievements.service';
-import { TeamdojoTestModule } from 'app/custom/test/test.module';
+
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
+
+class MockAccountService {
+  authenticate(identity: Account | null): void {
+    // do nothing
+  }
+
+  hasAnyAuthority(authorities: string[] | string): boolean {
+    return false;
+  }
+
+  identity(force?: boolean): Observable<Account | null> {
+    return new Observable();
+  }
+
+  isAuthenticated(): boolean {
+    return false;
+  }
+
+  getAuthenticationState(): Observable<Account | null> {
+    return new Observable();
+  }
+
+  getImageUrl(): string {
+    return '';
+  }
+}
+
+class MockActivatedRoute {
+  public queryParamMap = of(
+    convertToParamMap({
+      dimension: 1,
+      level: 2,
+      badge: 3,
+    })
+  );
+}
 
 describe('Component Tests', () => {
   describe('Team Achievements Component', () => {
     let comp: TeamsAchievementsComponent;
-    let fixture: ComponentFixture<TeamsAchievementsComponent>;
-    const buildEntity = Util.wrap;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
-        imports: [TeamdojoTestModule],
-        declarations: [TeamsAchievementsComponent],
-        providers: [TeamsAchievementsService],
-      })
-        .overrideTemplate(TeamsAchievementsComponent, '')
-        .compileComponents();
+        //declarations: [TeamsAchievementsComponent],
+        imports: [RouterTestingModule],
+        providers: [
+          TeamsAchievementsComponent,
+          { provide: AccountService, useClass: MockAccountService },
+          { provide: ActivatedRoute, useClass: MockActivatedRoute },
+        ],
+      });
 
-      fixture = TestBed.createComponent(TeamsAchievementsComponent);
-      comp = fixture.componentInstance;
+      comp = TestBed.inject(TeamsAchievementsComponent);
     });
 
     it('Should call load all on init', () => {
@@ -49,8 +88,34 @@ describe('Component Tests', () => {
       comp.badges = [];
       if (comp.team.participations) {
         comp.team.participations[0].levels = [
-          buildEntity(new Level(123), { dimensionId: 122 }),
-          buildEntity(new Level(124), { dimensionId: 122 }),
+          new Level(
+            123,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            new Dimension(122)
+          ),
+          new Level(
+            124,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            new Dimension(122)
+          ),
         ];
 
         // WHEN
@@ -87,7 +152,22 @@ describe('Component Tests', () => {
       entity.participations = [new Dimension(122)];
       comp.team = entity;
       if (comp.team.participations) {
-        comp.team.participations[0].levels = [buildEntity(new Level(123), { dimensionId: 122 })];
+        comp.team.participations[0].levels = [
+          new Level(
+            123,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            new Dimension(122)
+          ),
+        ];
         comp.badges = [];
         // WHEN
         comp.ngOnInit();
@@ -105,8 +185,38 @@ describe('Component Tests', () => {
       entity.participations = [new Dimension(122), new Dimension(123)];
       comp.team = entity;
       if (comp.team.participations) {
-        comp.team.participations[0].levels = [buildEntity(new Level(124), { dimensionId: 122 })];
-        comp.team.participations[1].levels = [buildEntity(new Level(125), { dimensionId: 123 })];
+        comp.team.participations[0].levels = [
+          new Level(
+            124,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            new Dimension(122)
+          ),
+        ];
+        comp.team.participations[1].levels = [
+          new Level(
+            125,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            new Dimension(123)
+          ),
+        ];
         comp.badges = [];
         // WHEN
         comp.ngOnInit();
@@ -121,11 +231,5 @@ describe('Component Tests', () => {
         expect(comp.team.participations[1].levels[0].id).toEqual(125);
       }
     });
-  });
-});
-*/
-describe('Placeholder for teams-achievements component test', () => {
-  it('remove', () => {
-    expect(1).toEqual(1);
   });
 });
