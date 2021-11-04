50bd8f147bdd4c739e7976b275d49a0d446c74f5
Amar Bolkan <amar.bolkan@iteratec.com>
2021-11-04 11:33:08 +0100

    Fix compiler errors in team-skill-vote component

---
+++ src/main/webapp/app/custom/entities/team-skill/team-skill-vote/team-skill-vote.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ITeamSkill } from 'app/entities/team-skill/team-skill.model';
+import { TeamSkillService } from 'app/entities/team-skill/service/team-skill.service';
+
+@Component({
+  selector: 'jhi-team-skill-vote',
+  templateUrl: './team-skill-vote.component.html',
+})
+export class TeamSkillVoteComponent implements OnInit {
+  teamSkill?: ITeamSkill;
+
+  constructor(private route: ActivatedRoute, private teamSkillService: TeamSkillService) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe(({ teamSkill }) => {
+      this.teamSkill = teamSkill.body ? teamSkill.body : teamSkill;
+    });
+  }
+
+  upVote(): void {
+    if (this.teamSkill?.vote) {
+      this.teamSkill.vote = this.teamSkill.vote + 1;
+      this.teamSkillService.update(this.teamSkill).subscribe(response => (this.teamSkill = response.body ? response.body : undefined));
+    }
+  }
+
+  downVote(): void {
+    if (this.teamSkill?.vote) {
+      this.teamSkill.vote = this.teamSkill.vote - 1;
+      this.teamSkillService.update(this.teamSkill).subscribe(response => (this.teamSkill = response.body ? response.body : undefined));
+    }
+  }
+}
