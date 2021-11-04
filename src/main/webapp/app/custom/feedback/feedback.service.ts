4f36086b117f39f295fad31f202a6de06aa287dc
Amar Bolkan <amar.bolkan@iteratec.com>
2021-11-04 13:39:08 +0100

    Add feedback feature from V1

---
+++ src/main/webapp/app/custom/feedback/feedback.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { SERVER_API_URL } from 'app/app.constants';
+import { IReport } from 'app/shared/model/report.model';
+import { map } from 'rxjs/operators';
+
+export type EntityResponseType = HttpResponse<IReport>;
+export type EntityArrayResponseType = HttpResponse<IReport[]>;
+
+@Injectable()
+export class FeedbackService {
+  private resourceUrl = SERVER_API_URL + 'api/reports';
+
+  constructor(private http: HttpClient) {}
+
+  create(report: IReport): Observable<EntityResponseType> {
+    const copy = this.convert(report);
+    return this.http
+      .post<IReport>(this.resourceUrl, copy, { observe: 'response' })
+      .pipe(map((res: EntityResponseType) => this.convertResponse(res)));
+  }
+
+  private convertResponse(res: EntityResponseType): EntityResponseType {
+    const body: IReport = this.convertItemFromServer(res.body);
+    return res.clone({ body });
+  }
+
+  /**
+   * Convert a returned JSON object to Feedback.
+   */
+  private convertItemFromServer(report: IReport): IReport {
+    const copy: IReport = Object.assign({}, report, {});
+    return copy;
+  }
+
+  /**
+   * Convert a Feedback to a JSON which can be sent to the server.
+   */
+  private convert(report: IReport): IReport {
+    const copy: IReport = Object.assign({}, report, {});
+    return copy;
+  }
+}
