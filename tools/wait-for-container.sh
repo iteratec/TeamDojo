42a0d3632d31f5ed67b1a8acaa6557126729fac9
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-02-15 12:23:46 +0100

    #60 Prevent endles loops waiting for service to come up
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- tools/wait-for-container.sh
+++ tools/wait-for-container.sh
@@ -7,30 +7,36 @@ set -euo pipefail
 #
 
 USAGE="$(basename "{0}") <service name> <compose file> <needle>"
+
 NAME="${1:-}"
 COMPOSE_FILE="${2:-}"
 NEEDLE="${3:-}"
+# How long to wait each check iteration in seconds:
+SLEEP=1
+# Multiplied with SLEEP is the timeout in seconds:
+TIMEOUT=150
 
 if [[ -z "${NAME}" ]]; then
-    echo >&2 "No name as first argument given!"
+    echo >&2 "ERROR: No name as first argument given!"
     echo >&2 "${USAGE}"
     exit 1
 fi
 
 if [[ -z "${COMPOSE_FILE}" ]]; then
-    echo >&2 "No compose file  as second argument given!"
+    echo >&2 "ERROR: No compose file  as second argument given!"
     echo >&2 "${USAGE}"
     exit 1
 fi
 
 if [[ -z "${NEEDLE}" ]]; then
-    echo >&2 "No needle as third argument given!"
+    echo >&2 "ERROR: No needle as third argument given!"
     echo >&2 "${USAGE}"
     exit 1
 fi
 
 echo -n "Waiting for ${NAME}"
 
+COUNTER=0
 while true; do
     echo -n '.'
     # The 'true' at the end is to prevent script abortion because grep exits with 1 if nothing matches.
@@ -42,5 +48,12 @@ while true; do
         break
     fi
 
-    sleep 1
+    if [[ $COUNTER -gt $TIMEOUT ]]; then
+        echo >&2 "ERROR: Timeout exceeded! ${NAME} didn't came up."
+        echo >&2 "ERROR: Can't find needle in log output: '${NEEDLE}'"
+        exit 1
+    fi
+
+    sleep $SLEEP
+    (( COUNTER++ )) || true
 done
