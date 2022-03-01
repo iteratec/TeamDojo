8943c64a5dc8f1404369ba2ba65c686f097f849d
Amar Bolkan <amar.bolkan@iteratec.com>
2022-03-01 14:26:18 +0100

    Use breadcrumb trail component in navbar
    Also clean up redundant styling and unused imports + variables

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {
   Router,
   // start
-  ActivatedRoute,
+  // ActivatedRoute,
   // end
 } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
@@ -15,9 +15,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { LoginService } from 'app/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
 import { EntityNavbarItems } from 'app/entities/entity-navbar-items';
-// ### Modification-Start ###
-import { IBreadcrumb } from 'app/custom/entities/breadcrumb/breadcrumb.model';
-// ### Modification-End ###
+
 @Component({
   selector: 'jhi-navbar',
   templateUrl: './navbar.component.html',
@@ -34,11 +32,6 @@ export class NavbarComponent implements OnInit {
   account: Account | null = null;
   entitiesNavbarItems: any[] = [];
 
-  // ### Modification-Start ###
-  organisationName = '';
-  breadcrumbs: IBreadcrumb[] = [];
-  // ### Modification-End ###
-
   constructor(
     private loginService: LoginService,
     private translateService: TranslateService,
@@ -47,7 +40,8 @@ export class NavbarComponent implements OnInit {
     private profileService: ProfileService,
     // ### Modification-Start ###
     private router: Router,
-    private route: ActivatedRoute // private breadcrumbService: BreadcrumbService, // ### Modification-End ###
+    //private route: ActivatedRoute
+    // ### Modification-End ###
   ) {
     if (VERSION) {
       this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
@@ -55,16 +49,6 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // ### Modification-Start ###
-    // this.breadcrumbs = this.breadcrumbService.getCurrentBreadcrumb();
-    /* this.breadcrumbService.breadcrumbChanged.subscribe(breadcrumb => {
-      this.breadcrumbs = this.breadcrumbService.getCurrentBreadcrumb();
-    });
-
-     this.route.data.subscribe(({ organisation }) => {
-      this.organisationName = organisation.name;
-    }); */
-    // ### Modification-End ###
     this.entitiesNavbarItems = EntityNavbarItems;
     this.profileService.getProfileInfo().subscribe(profileInfo => {
       this.inProduction = profileInfo.inProduction;
