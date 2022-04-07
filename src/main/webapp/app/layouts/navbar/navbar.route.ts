07e95f2fd9a0fdbf70228d8632b0c32134e31f67
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-04-07 20:12:51 +0200

    #5 Add missing modifiaction markers in generated code
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/webapp/app/layouts/navbar/navbar.route.ts
+++ src/main/webapp/app/layouts/navbar/navbar.route.ts
@@ -1,7 +1,9 @@
 import { Route } from '@angular/router';
 
 import { NavbarComponent } from './navbar.component';
+// ### MODIFICATION-START ###
 import { OrganisationResolve } from '../../custom/common.resolver';
+// ### MODIFICATION-END ###
 
 export const navbarRoute: Route = {
   path: '',
