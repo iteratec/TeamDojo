d9b850163341f20ac029eed6be893d44bb213b90
Amar Bolkan <amar.bolkan@iteratec.com>
2022-05-16 15:19:43 +0200

    Issue #31: Use image hash as cacheBuster
    Instead of using the Title as a cacheBuster and putting restrictions
    on how an image entity can be named (a valid URI string cant contain spaces)
    the hash of the image should be used.

--- src/main/webapp/app/custom/shared/pipe/image-url.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/image-url.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'imageUrl' })
 export class ImageUrlPipe implements PipeTransform {
-  transform(imageId: string | number | undefined, size: string, cacheBuster?: string): string {
+  transform(imageId: string | number | undefined, size: string, cacheBuster?: string | null): string {
     if (imageId) {
       let imgUrl = `/api/images/${imageId}/content?size=${size}`;
 
--- src/main/webapp/app/custom/shared/team-image/team-image.component.ts
+++ src/main/webapp/app/custom/shared/team-image/team-image.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, SimpleChange } from '@angular/core';
 import { ITeam } from 'app/entities/team/team.model';
 import dayjs from 'dayjs/esm';
+import { ImageService } from '../../../entities/image/service/image.service';
 
 @Component({
   selector: 'jhi-team-image',
@@ -16,11 +17,33 @@ export class TeamImageComponent {
   @Input() hasOverlay = false;
   @Input() showExpiredLabel = true;
 
+  constructor(protected imageService: ImageService) {
+    this.queryImageHash(this.team);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (Object.prototype.hasOwnProperty.call(changes, 'team')) {
+      const teamChange: SimpleChange = changes['team'];
+
+      this.queryImageHash(teamChange.currentValue);
+    }
+  }
+
   isExpired(team?: ITeam): boolean {
     if (team?.expirationDate != null) {
       return team.expirationDate.isBefore(dayjs(), 'day');
     }
 
     return false;
+  }
+
+  private queryImageHash(team?: ITeam): void {
+    if (team?.image?.id) {
+      this.imageService.find(team.image.id).subscribe(response => {
+        if (this.team?.image) {
+          this.team.image.hash = response.body?.hash;
+        }
+      });
+    }
   }
 }
