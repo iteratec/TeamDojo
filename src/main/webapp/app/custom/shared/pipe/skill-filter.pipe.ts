a2ac1714b03194ce15a0b15c331adc9557bc444c
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-05-30 21:29:52 +0200

    #8 Use translated title property in skil filter pipe
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/webapp/app/custom/shared/pipe/skill-filter.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/skill-filter.pipe.ts
@@ -1,9 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ISkill } from 'app/entities/skill/skill.model';
+import { TranslateModelService } from '../translate-model/translate-model.service';
 
 @Pipe({ name: 'skillFilter' })
 export class SkillFilterPipe implements PipeTransform {
+  constructor(private translation: TranslateModelService) {}
+
   transform(skills: ISkill[], searchString: string): ISkill[] {
-    return skills.filter(skill => (skill.titleEN ?? '').toLowerCase().includes(searchString.toLowerCase()));
+    const normalizedSearchString = searchString.toLowerCase();
+    return skills.filter(skill => {
+      const normalizedTitle = this.translation.translateProperty(skill, 'title').toLowerCase();
+      return normalizedTitle.includes(normalizedSearchString);
+    });
   }
 }
