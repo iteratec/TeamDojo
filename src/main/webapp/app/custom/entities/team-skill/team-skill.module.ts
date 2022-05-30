76f55cf59a2e5d4878762168e24076b62f38a81b
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-05-30 14:12:20 +0200

    #8 Use new translation pipe in component templates
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/webapp/app/custom/entities/team-skill/team-skill.module.ts
+++ src/main/webapp/app/custom/entities/team-skill/team-skill.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from 'app/shared/shared.module';
 
 import { CustomTeamSkillRoutingModule } from 'app/custom/entities/team-skill/route/team-skill-routing.module';
 import { TeamSkillVoteComponent } from 'app/custom/entities/team-skill/team-skill-vote/team-skill-vote.component';
+import { TeamdojoSharedCommonModule } from '../../shared/shared-common.module';
 
 @NgModule({
-  imports: [SharedModule, CustomTeamSkillRoutingModule],
+  imports: [SharedModule, CustomTeamSkillRoutingModule, TeamdojoSharedCommonModule],
   declarations: [TeamSkillVoteComponent],
   exports: [TeamSkillVoteComponent],
 })
--- src/main/webapp/app/entities/comment/comment.module.ts
+++ src/main/webapp/app/entities/comment/comment.module.ts
@@ -5,9 +5,10 @@ import { CommentDetailComponent } from './detail/comment-detail.component';
 import { CommentUpdateComponent } from './update/comment-update.component';
 import { CommentDeleteDialogComponent } from './delete/comment-delete-dialog.component';
 import { CommentRoutingModule } from './route/comment-routing.module';
+import { TeamdojoSharedCommonModule } from '../../custom/shared/shared-common.module';
 
 @NgModule({
-  imports: [SharedModule, CommentRoutingModule],
+  imports: [SharedModule, CommentRoutingModule, TeamdojoSharedCommonModule],
   declarations: [CommentComponent, CommentDetailComponent, CommentUpdateComponent, CommentDeleteDialogComponent],
   entryComponents: [CommentDeleteDialogComponent],
 })
