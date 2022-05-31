08eab4b32ef81f3a92efed904d291798233f9d08
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-05-31 21:12:02 +0200

    #8 Use same naming as in generated frontend services
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/webapp/app/custom/teams/teams-skills.service.ts
+++ src/main/webapp/app/custom/teams/teams-skills.service.ts
@@ -27,7 +27,7 @@ export class TeamsSkillsService {
       .pipe(
         map(res => {
           if (res.body) {
-            return this.convertItemFromServer(res.body);
+            return this.convertDateFromServer(res.body);
           }
 
           return new AchievableSkill();
@@ -56,14 +56,14 @@ export class TeamsSkillsService {
   }
 
   updateAchievableSkill(teamId: number, skill: IAchievableSkill): Observable<EntityResponseType> {
-    const copy = this.convert(skill);
+    const copy = this.convertDateFromClient(skill);
     return this.http
       .put<IAchievableSkill>(`${this.resourceUrl}/${teamId}/achievable-skills`, copy, { observe: 'response' })
       .pipe(map((res: EntityResponseType) => this.convertResponse(res)));
   }
 
   private convertResponse(res: EntityResponseType): EntityResponseType {
-    const body: IAchievableSkill = res.body ? this.convertItemFromServer(res.body) : new AchievableSkill();
+    const body: IAchievableSkill = res.body ? this.convertDateFromServer(res.body) : new AchievableSkill();
     return res.clone({ body });
   }
 
@@ -71,7 +71,7 @@ export class TeamsSkillsService {
     const jsonResponse: IAchievableSkill[] = res.body ? res.body : [];
     const body: IAchievableSkill[] = [];
     for (let i = 0; i < jsonResponse.length; i++) {
-      body.push(this.convertItemFromServer(jsonResponse[i]));
+      body.push(this.convertDateFromServer(jsonResponse[i]));
     }
     return res.clone({ body });
   }
@@ -79,7 +79,7 @@ export class TeamsSkillsService {
   /**
    * Convert a returned JSON object to Skill.
    */
-  private convertItemFromServer(achievableSkill: IAchievableSkill): IAchievableSkill {
+  private convertDateFromServer(achievableSkill: IAchievableSkill): IAchievableSkill {
     return Object.assign({}, achievableSkill, {
       achievedAt: achievableSkill.achievedAt != null ? moment(achievableSkill.achievedAt) : achievableSkill.achievedAt,
       verifiedAt: achievableSkill.verifiedAt != null ? moment(achievableSkill.verifiedAt) : achievableSkill.verifiedAt,
@@ -89,7 +89,7 @@ export class TeamsSkillsService {
   /**
    * Convert a Skill to a JSON which can be sent to the server.
    */
-  private convert(achievableSkill: IAchievableSkill): IAchievableSkill {
+  private convertDateFromClient(achievableSkill: IAchievableSkill): IAchievableSkill {
     return Object.assign({}, achievableSkill, {
       completedAt: achievableSkill.achievedAt?.isValid() ? achievableSkill.achievedAt.toJSON() : null,
       verifiedAt: achievableSkill.verifiedAt?.isValid() ? achievableSkill.verifiedAt.toJSON() : null,
