3d5b2eb7b0b03a5db6b2599bb8a1f15c306c2cad
Sven Strittmatter <sven.strittmatter@iteratec.com>
2022-06-01 12:59:43 +0200

    #8 Use new private keyword in TS
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/main/webapp/app/custom/shared/pipe/achievable-skill-sort.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/achievable-skill-sort.pipe.ts
@@ -6,14 +6,14 @@ type sortArg = keyof IAchievableSkill;
 @Pipe({ name: 'achievableSkillSort' })
 export class AchievableSkillSortPipe implements PipeTransform {
   transform(skills: IAchievableSkill[], sortProperty: sortArg): IAchievableSkill[] {
-    const sortPropertyNullSafe = this._defaultString(sortProperty);
+    const sortPropertyNullSafe = this.defaultString(sortProperty);
     const reverse = ['score', 'rateCount'].includes(sortPropertyNullSafe) ? -1 : 1;
     return Array.from(skills).sort(
-      (skill1, skill2) => reverse * this._defaultString(skill1[sortProperty]).localeCompare(this._defaultString(skill2[sortProperty]))
+      (skill1, skill2) => reverse * this.defaultString(skill1[sortProperty]).localeCompare(this.defaultString(skill2[sortProperty]))
     );
   }
 
-  _defaultString(smth: any): string {
+  private defaultString(smth: any): string {
     return smth || smth === 0 ? String(smth) : String();
   }
 }
