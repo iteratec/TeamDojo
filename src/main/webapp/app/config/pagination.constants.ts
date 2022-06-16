35e087331dff6cad0f6f72d92bd485e6df270e3d
Amar Bolkan <amar.bolkan@iteratec.com>
2022-06-16 16:50:05 +0200

    Issue #112: Add pagination constants for different entities

--- src/main/webapp/app/config/pagination.constants.ts
+++ src/main/webapp/app/config/pagination.constants.ts
@@ -2,3 +2,14 @@ export const ITEMS_PER_PAGE = 20;
 export const ASC = 'asc';
 export const DESC = 'desc';
 export const SORT = 'sort';
+
+// ### MODIFICATION-START ###
+export const TEAMS_PER_PAGE = 100;
+export const TEAM_SKILLS_PER_PAGE = 100;
+export const SKILLS_PER_PAGE = 100;
+export const LEVEL_SKILLS_PER_PAGE = 100;
+export const BADGE_SKILLS_PER_PAGE = 100;
+export const COMMENTS_PER_PAGE = 100;
+export const TEAM_GROUPS_PER_PAGE = 100;
+export const TRAININGS_PER_PAGE = 100;
+// ### MODIFICATION-END ###
