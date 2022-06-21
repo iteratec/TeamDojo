84c9c4955b155bbcc0373675848afda757b867e7
Amar Bolkan <amar.bolkan@iteratec.com>
2022-06-21 10:03:07 +0200

    Issue #115: Fix inconsistent search function in regards to language
    This patch completes  a FIXME left over from Issue #8

--- src/main/webapp/app/custom/shared/pipe/achievable-skill-filter.pipe.ts
+++ src/main/webapp/app/custom/shared/pipe/achievable-skill-filter.pipe.ts
@@ -1,10 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IAchievableSkill } from 'app/custom/entities/achievable-skill/achievable-skill.model';
+import { TranslateModelService } from '../translate-model/translate-model.service';
 
-@Pipe({ name: 'achievableSkillFilter' })
+@Pipe({
+  name: 'achievableSkillFilter',
+  pure: false, // Must not be pure so that already rendered components will be rendered again after language switch.
+})
 export class AchievableSkillFilterPipe implements PipeTransform {
+  constructor(private translation: TranslateModelService) {}
+
   transform(skills: IAchievableSkill[], searchString: string): IAchievableSkill[] {
-    // FIXME: #8 Localize title attribute here.
-    return skills.filter(skill => (skill.titleEN ?? '').toLowerCase().includes(searchString.toLowerCase()));
+    const normalizedSearchString = searchString.toLowerCase();
+    return skills.filter(skill => {
+      const normalizedTitle = this.translation.translateProperty(skill, 'title').toLowerCase();
+      return normalizedTitle.includes(normalizedSearchString);
+    });
   }
 }
