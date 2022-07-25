6c1ac4e84bff892e42014fc4a39098944d697e3d
Rami Souai <rami.souai@iteratec.com>
2022-07-25 10:26:12 +0200

    #103: Add missing imports to test
    Signed-off-by: Rami Souai <rami.souai@iteratec.com>

--- src/main/webapp/app/custom/shared/skill-card/skill-card.component.spec.ts
+++ src/main/webapp/app/custom/shared/skill-card/skill-card.component.spec.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { LocalStorageMock } from 'app/custom/overview/teams/overview-teams.component.spec';
 import { TeamsSelectionService } from 'app/custom/teams-selection/teams-selection.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 import { SkillCardComponent } from './skill-card.component';
 
@@ -10,8 +12,9 @@ describe('SkillCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [SkillCardComponent],
-      providers: [TeamsSelectionService, HttpClient],
+      providers: [TeamsSelectionService, { provide: LocalStorageService, useClass: LocalStorageMock }],
     }).compileComponents();
     teamsSelectionService = TestBed.inject(TeamsSelectionService);
     component = new SkillCardComponent(teamsSelectionService);
