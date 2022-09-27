ac063a1cf9c342af26b1d83708323d324035e6f0
Amar Bolkan <amar.bolkan@iteratec.com>
2022-09-27 10:37:10 +0200

    Issue #149: Increase Teamskills per page

--- src/main/webapp/app/config/pagination.constants.ts
+++ src/main/webapp/app/config/pagination.constants.ts
@@ -8,7 +8,7 @@ export const SORT = 'sort';
 
 // ### MODIFICATION-START ###
 export const TEAMS_PER_PAGE = 500;
-export const TEAM_SKILLS_PER_PAGE = 100;
+export const TEAM_SKILLS_PER_PAGE = 5000;
 export const SKILLS_PER_PAGE = 100;
 export const LEVEL_SKILLS_PER_PAGE = 100;
 export const BADGE_SKILLS_PER_PAGE = 100;
